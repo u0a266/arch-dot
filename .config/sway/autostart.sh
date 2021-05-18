db12eee86349b83e5698e13a97f47f02ac290eee
u0a266 <72482919+u0a266@users.noreply.github.com>
2021-05-18 12:05:36 +0900

    Update

---
+++ .config/sway/autostart.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+fcitx5 &
+volumeicon &
+parcellite &
+/usr/lib/kdeconnectd &
+blueberry-tray &
+firewall-applet &
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+kdeconnect-indicator &
+nm-applet &
+imwheel &
+keepassxc &
+mako &
+gammastep &
