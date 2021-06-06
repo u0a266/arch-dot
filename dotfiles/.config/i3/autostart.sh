58b75ec5632651894a527bc7a48ac7d90e789b99
u0a266 <72482919+u0a266@users.noreply.github.com>
2021-06-06 17:24:55 +0900

    Update

--- dotfiles/.config/i3/autostart.sh
+++ dotfiles/.config/i3/autostart.sh
@@ -6,7 +6,6 @@ blueberry-tray &
 firewall-applet &
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 /usr/lib/xfce4/notifyd/xfce4-notifyd &
-kdeconnect-indicator &
 nm-applet &
 imwheel &
 keepassxc &
