115bb7ab9594de0423e184564ca30237532bf3d2
u0a266 <72482919+u0a266@users.noreply.github.com>
2021-07-04 10:33:44 +0900

    Upd

--- dotfiles/.config/sway/autostart.sh
+++ dotfiles/.config/sway/autostart.sh
@@ -3,6 +3,6 @@ fcitx5 &
 blueberry-tray &
 firewall-applet &
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
-keepassxc &
+gtk-launch org.keepassxc.KeePassXC.desktop
 mako &
 gammastep &
